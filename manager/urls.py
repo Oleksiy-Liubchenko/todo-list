8773a67432f3b1575014f9c6311f6a0b810c53b2
Oleksiy Liubchenko <alexeylubchenko75@gmail.com>
2023-04-19 00:34:17 +0300

    Update url naming in urls.py

--- manager/urls.py
+++ manager/urls.py
@@ -7,7 +7,8 @@ from manager.views import (
     TaskUpdateView,
     TagCreationView,
     TagUpdateView,
-    TagDeleteView
+    TagDeleteView,
+    TaskUpdateStatusView
 )
 
 app_name = "manager"
@@ -16,6 +17,7 @@ urlpatterns = [
     path("", TaskListView.as_view(), name="tasks-list"),
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
     path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/update/status/", TaskUpdateStatusView.as_view(), name="task-status-update"),
     path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
     path("tags/", TagListView.as_view(), name="tags-list"),
     path("tags/create/", TagCreationView.as_view(), name="tag-create"),
