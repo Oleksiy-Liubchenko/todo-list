9f81ba6de1b5e3493a3ed0d2568d3859b47f4821
Oleksiy Liubchenko <alexeylubchenko75@gmail.com>
2023-04-18 20:03:56 +0300

    Add tag & task creation views & update urls

--- manager/urls.py
+++ manager/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path
-from manager.views import TaskListView, TagListView
+from manager.views import (
+    TaskListView,
+    TagListView,
+    TaskCreateView,
+    TaskDeleteView,
+    TaskUpdateView,
+    TagCreationView
+)
 
 app_name = "manager"
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="tasks-list"),
-    path("tags/", TagListView.as_view(), name="tags-list")
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("tags/", TagListView.as_view(), name="tags-list"),
+    path("tags/create/", TagCreationView.as_view(), name="tag-create"),
+
 ]
--- manager/views.py
+++ manager/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
+from manager.forms import TaskCreationForm, TagCreationForm
 from manager.models import Task, Tag
 
 
@@ -9,7 +11,31 @@ class TaskListView(generic.ListView):
     template_name = "manager/index.html"
 
 
+class TaskCreateView(generic.CreateView):
+    model = Task
+    template_name = "manager/task_form.html"
+    success_url = reverse_lazy("manager:tasks-list")
+    form_class = TaskCreationForm
+
+
+class TaskDeleteView(generic.DeleteView):
+    pass
+
+
+class TaskUpdateView(generic.UpdateView):
+    pass
+
+
 class TagListView(generic.ListView):
     model = Tag
     template_name = "manager/tag_list.html"
 
+
+class TagCreationView(generic.CreateView):
+    model = Tag
+    template_name = "manager/tag_form.html"
+    success_url = reverse_lazy("manager:tasks-list")
+    form_class = TagCreationForm
+
+# class TagUpdateView(generic.UpdateView):
+#     model =
