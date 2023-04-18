589a1c7082cf905e6f3c9c967ea329b770efa75c
Oleksiy Liubchenko <alexeylubchenko75@gmail.com>
2023-04-18 20:30:17 +0300

    Add tag & task create/delete urls to urls.py

--- manager/urls.py
+++ manager/urls.py
@@ -5,7 +5,9 @@ from manager.views import (
     TaskCreateView,
     TaskDeleteView,
     TaskUpdateView,
-    TagCreationView
+    TagCreationView,
+    TagUpdateView,
+    TagDeleteView
 )
 
 app_name = "manager"
@@ -13,9 +15,11 @@ app_name = "manager"
 urlpatterns = [
     path("", TaskListView.as_view(), name="tasks-list"),
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
-    path("tasks/update/", TaskUpdateView.as_view(), name="task-update"),
-    path("tasks/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
     path("tags/", TagListView.as_view(), name="tags-list"),
     path("tags/create/", TagCreationView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
 
 ]
