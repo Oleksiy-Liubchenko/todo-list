a6e68ca151be32f3fff2e7d27e281fe02e1eebc7
Oleksiy Liubchenko <alexeylubchenko75@gmail.com>
2023-04-18 15:57:59 +0300

    Add models.py with migrations

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, blank=False)
+
+
+class Task(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    status = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+
