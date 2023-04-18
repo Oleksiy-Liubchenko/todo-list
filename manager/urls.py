e10bbd47d86acf976edcad20ff37bbb389a353bd
Oleksiy Liubchenko <alexeylubchenko75@gmail.com>
2023-04-18 16:47:00 +0300

    Add TagListView & urls + template

--- manager/urls.py
+++ manager/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from manager.views import TaskListView
+from manager.views import TaskListView, TagListView
 
 app_name = "manager"
 
 urlpatterns = [
-    path("", TaskListView.as_view())
+    path("", TaskListView.as_view(), name="tasks-list"),
+    path("tags/", TagListView.as_view(), name="tags-list")
 ]
--- manager/views.py
+++ manager/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.views import generic
 
-from manager.models import Task
+from manager.models import Task, Tag
 
 
 class TaskListView(generic.ListView):
     model = Task
     template_name = "manager/index.html"
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    template_name = "manager/tag_list.html"
+
