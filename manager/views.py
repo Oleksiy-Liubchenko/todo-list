eead5868f602daa2ffb45504dbc942e4ea79d48e
Oleksiy Liubchenko <alexeylubchenko75@gmail.com>
2023-04-19 00:35:49 +0300

    Update status changing view for Task model

--- manager/views.py
+++ manager/views.py
@@ -1,5 +1,6 @@
 from django.urls import reverse_lazy
-from django.views import generic
+from django.views import generic, View
+from django.shortcuts import get_object_or_404, redirect
 
 from manager.forms import TaskCreationForm, TagCreationForm
 from manager.models import Task, Tag
@@ -30,6 +31,21 @@ class TaskUpdateView(generic.UpdateView):
     form_class = TaskCreationForm
 
 
+class TaskUpdateStatusView(View):
+    def get(self, request, pk):
+        task = get_object_or_404(Task, id=pk)
+        task.status = not task.status
+        task.save()
+
+        return redirect("manager:task-list")
+
+    def post(self, request, pk):
+        task = get_object_or_404(Task, id=pk)
+        task.status = request.POST.get("status", not task.status)
+        task.save()
+        return redirect("manager:tasks-list")
+
+
 class TagListView(generic.ListView):
     model = Tag
     template_name = "manager/tag_list.html"
@@ -38,19 +54,18 @@ class TagListView(generic.ListView):
 class TagCreationView(generic.CreateView):
     model = Tag
     template_name = "manager/tag_form.html"
-    success_url = reverse_lazy("manager:tasks-list")
+    success_url = reverse_lazy("manager:tags-list")
     form_class = TagCreationForm
 
 
 class TagUpdateView(generic.UpdateView):
     model = Tag
     template_name = "manager/tag_form.html"
-    success_url = reverse_lazy("manager:tasks-list")
+    success_url = reverse_lazy("manager:tags-list")
     form_class = TagCreationForm
 
 
 class TagDeleteView(generic.DeleteView):
     model = Tag
     template_name = "manager/tag_confirm_delete.html"
-    success_url = reverse_lazy("manager:tasks-list")
-    form_class = TagCreationForm
+    success_url = reverse_lazy("manager:tags-list")
